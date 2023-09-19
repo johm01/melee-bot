c1177d5d9c23a9754d30cb656d9056b75987f93c
johm01 <iplaybdo01@gmail.com>
2023-09-18 19:50:37 -0500

    basic function of set-finder complete

--- find_set.py
+++ find_set.py
@@ -1,15 +1,9 @@
 from googleapiclient.discovery import build
-from bs4 import BeautifulSoup
-import requests
 
-key = 'AIzaSyDXq73Ca4uGdkP4z9SqkCJZHGMi60qFRlo'
+key = ''
 service = 'youtube'
 service_ver = 'v3'
 # Create a build func
-
-def sort_views(views: list):
-    pass
-
 
 def get_views(url):
     tube = build(service,service_ver,developerKey=key)
@@ -19,7 +13,7 @@ def get_views(url):
             id=url
             ).execute()
 
-    return r['items'][0]['statistics']['viewCount']
+    return int(r['items'][0]['statistics']['viewCount'])
 
 def find_set(term: str):
     tube = build(service,service_ver,developerKey=key)
@@ -30,17 +24,20 @@ def find_set(term: str):
             maxResults=10
             ).execute()
     
-    ids = []
+
     views = []
+    ids = []
+    d = {}
     for result in search.get('items',[]):
         if result['id']['kind'] == 'youtube#video':
-            ids.append(result['id']['videoId']
+            ids.append(result['id']['videoId'])
             views.append(get_views(result['id']['videoId']))
     
-    # Sort views for most views
-    v_sorted = sort
+    for i in range(len(views)):
+        d[views[i]] = ids[i]
 
-    return ids[0]
+    print(d)
+    return "https://www.youtube.com/watch?v="+ids[0]
 
-set_1 = find_set('ssbm yoshi vs fox')
+set_1 = find_set('SSBM tournament peach vs fox')
 print(set_1)
