ba58a3222f8053eae2589a75889e4aa185b3604a
john <97820338+johm01@users.noreply.github.com>
2023-09-26 11:22:17 -0500

    Update get-frames.py

--- get-frames.py
+++ get-frames.py
@@ -1,23 +1,26 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = "https://meleeframedata.com/fox"
+char = 'fox'
+url = f'https://meleeframedata.com/{char}'
 r = requests.get(url)
 
 soup = BeautifulSoup(r.content,'html5lib')
-#print(soup)
 table = soup.findAll(class_="move-container")
 table2 = soup.findAll("div",attrs={'class':"special-movename"})
 
-keyword = {
-        "Jab":['jab','Jab']
+keywords = {
+        "Jab":['jab','Jab','jab 1','jab1'],
+        "U Tilt":['utilt','u-tilt','up-tilt','u tilt'],
+        "D Tilt":['d-tilt','dtilt','down tilt','down-tilt'],
+        "F Tilt":['f-tilt','forward tilt','ftilt','f tilit'],
         }
 
 def find_move(movename: str):
     can_find_move = None
     string_to_use = None
-    for keys in keyword:
-        for t in keyword[keys]:
+    for keys in keywords:
+        for t in keywords[keys]:
             if movename == t:
                 can_find_move = True
                 string_to_use = keys
