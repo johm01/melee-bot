0c2003f339fd37c664931e581220f63fb392c78c
johm01 <iplaybdo01@gmail.com>
2023-09-26 19:12:06 -0500

    commit

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import json
 from find_set import *
 from discord.ext import commands 
 
-
 #todo-list 
 
 role_emojis = ['Shiek','Mewtwo','Yoshi','Kirby','Game & Watch','Bowser','Ganondorf','Peach','Marth','Roy','Ice Climbers','DK'
@@ -22,16 +21,6 @@ client = commands.Bot(command_prefix='?',intents=intents)
 
 # Bot id
 bot_id = '1150638542062637086'
-
-'''
-@client.event
-async def on_message(message):
-    guild_id = message.guild_id
-    guild = discord.utils.find(lambda g:g.id==guild_id,client.guilds)
-    if message.author.id != bot_id:
-        await message.reply(message.created_at)
-        pass
-'''
 
 @client.event
 async def on_raw_reaction_add(payload):
@@ -77,38 +66,4 @@ async def find_set(ntx: str,match: str):
     msg = get_set("SSBM tournament "+match)
     await ntx.send(msg)
 
-def load_moveset(char: str):
-    char_data = f'./framedata-json/{char}.json'
-    with open(char_data,encoding="utf-8") as f:
-        data = json.load(f)
-        data = list(data.items())
-    
-    if data:
-        return data
-    else:
-        return None 
-
-def get_move(data,move: str):
-    move_found = None
-    
-    for i in range(len(data)):
-        if data[i][0] == move:
-            msg = data[i][1]['totalFrames']
-            # Total frames
-            print(data[i][1]['totalFrames'])
-           
-            # A moves properties  
-            print(data[i][0])
-
-            # All hit frames for a move 
-            for i in data[i][1]['hitFrames']:
-                print(i['start'])
-                print(i['end'])
-        else:
-            move_found = False
-
-    return msg
-         
-move_set = load_moveset('Fox')
-print(get_move(move_set,'dair'))
 client.run(token)
